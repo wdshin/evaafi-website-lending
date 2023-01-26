703ed95ecc0b9504aebff3f336ac3a550e971a01
Eugene <eanisei@gmail.com>
2023-01-26 12:32:22 +0100

    link changed

--- src/components/Layout/popup.js
+++ src/components/Layout/popup.js
@@ -7,7 +7,7 @@ const Popup = () => {
         <React.Fragment>
             <UncontrolledAlert className="card cookie-popup shadow rounded py-3 px-4">
                 <p className="text-muted mb-0 fs-6">This website uses cookies to provide you with a great user experience. By using it,
-                    you accept our <Link to="https://evaa.finance" target="_blank" rel="noopener noreferrer" className="text-success h6">use of cookies</Link></p>
+                    you accept our <Link to="#" target="_blank" rel="noopener noreferrer" className="text-success h6">use of cookies</Link></p>
                 <div className="cookie-popup-actions text-end">
                     {/* <button>
                         <i className="uil uil-times text-dark fs-4"></i>
